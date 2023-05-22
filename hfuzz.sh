9ae10a55565529518dd588be364a35bd485b2bce
meweez <meweez@meweezs-MacBook-Pro.local>
2023-05-22 10:24:53 +0330

    big update

--- hfuzz.sh
+++ hfuzz.sh
@@ -16,38 +16,82 @@ printf "
 | | | |  _| |_| |/ / / / 
 |_| |_|_|  \__,_/___/___|
                          
-        ${cyan}Developed by MHA${NC}             
+        ${cyan}Developed by MHA & Meweez${NC}             
                  ${yellow}mha4065.com${NC}
 
 "
 
-usage() { echo -e "${yellow}Usage:${NC} ./hfuzz.sh -d domain.tld -w wordlist.txt -s subdomain.txt [-t <int>] [-m <int>] [-f <int>]" 1>&2; exit 1; }
+usage() {
+    echo -e "${yellow}Usage:${NC} ./hfuzz.sh   
+    [-d (domain just for append mode)] 
+    [-s (For TLS (By default, the scheme is http) )] {-i|-I} [-w wordlist] [-S] [-t] [-m] [-f]
 
-while getopts "d:s:w:t:m:f:" flag
-do
-    case "${flag}" in
-        d) domain=${OPTARG#*//};;
-        s) subdomain="$OPTARG";;
-        w) wordlist="$OPTARG";;
-        t) thread="-t $OPTARG";;
-        m) match_code="-mc $OPTARG";;
-        f) filter_code="-fc $OPTARG";;
-        \? ) usage;;
-        : ) usage;;
-		*) usage;;
+    [-i (just a single IP -> 1.2.3.4)]
+    [-I (a list of IP -> ip.txt)]
+    [-w (a wordlist -> words.txt)]
+    [-S (a wordlist of subdomains -> subdomains.txt)]
+    [-t thread]
+    [-m match-codes]
+    [-f filter-codes]
+    " 1>&2;
+    exit 1;
+}
+
+ip=
+protocol="http"
+domain=
+ips=
+wordlist=
+swordlist=
+
+matchcode=" -mc "
+filtercode=" -fc "
+thread=" -t "
+
+while getopts "h:d:i:I:w:t:m:f:S:s" o; do
+    case $o in
+        i)
+            ip=$OPTARG
+            ;;
+        m)
+            matchcode="$matchcode$OPTARG"
+            ;;
+        f)
+            filtercode="$filtercode$OPTARG"
+            ;;
+        d)
+            domain=$OPTARG
+            ;;
+        I)
+            ips=$OPTARG
+            ;;
+        t)
+            thread="$thread$OPTARG"
+            ;;
+        w)
+            wordlist=$OPTARG
+            ;;
+        s)
+            protocol="https"
+            ;;
+        S)
+            swordlist=$OPTARG
+            ;;
+        h | *)
+            echo "$o"
+            usage
+            ;;
     esac
 done
 
-if [[ -z "${domain}" ]] || [[ -z "${subdomain}" ]] || [[ -z "${wordlist}" ]]; then
-  usage
-fi
+ 
 
 # Check results/domain is exist or not
 #=======================================================================
-if [ ! -d "results" ]; then
-    mkdir "results"
-    if [ ! -d "results/$domain" ]; then
-    	mkdir "results/$domain"
+if [ ! -d "hfuzz-result" ]; then
+    mkdir "hfuzz-results"
+    if [ ! -d "hfuzz-results/$domain" ]; then
+    	mkdir "hfuzz-results/$domain"
     fi
 fi
 #=======================================================================
@@ -67,6 +111,19 @@ fi
 echo -e "   ${green}[+]${NC} All requirements are installed :)"
 #=======================================================================
 
+#get options ready
+options=
+if [ ! "$matchcode" = " -mc " ]; then
+    options="$matchcode "
+fi
+if [ ! "$filtercode" = " -fc " ]; then
+    options="$options$filtercode "
+fi
+if [ ! "$thread" = " -t " ]; then
+    options="$options$thread "
+fi
+#echo -e "$options"
+
 
 echo
 echo -e "${blue}[!]${NC} Host header fuzzing : "
@@ -74,19 +131,92 @@ echo -e "${blue}[!]${NC} Host header fuzzing : "
 # Step 1 : only static wordlist as host header
 #=======================================================================
 echo -e "   ${green}[+]${NC} Fuzzing step 1 - fuzzing method -> host: <word>"
-ffuf -w $wordlist -u "https://domaint.tld" -H "host: FUZZ" -H 'User-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:42.0) Gecko/20100101 Firefox/42.0' $thread $match_code $filter_code -s -o results/$domain/step1.txt
+if ([ -z "$ip" ] && [ -z "$ips" ]) || ([ -z "$wordlist" ]);then
+    echo -e "Please define Ip, wordlist"
+    usage
+else
+    if [ -z "$ip" ];then
+        # fuzz list of ips with the wordlist
+        for i in $(cat "$ips");do
+            # with this filter we want to remove those are showing the same website as default
+            filterrespsize=$(curl -s -k -I "$protocol://$i" | grep -i Content-Length | cut -f2 -d' ' | tr -d '\r\n')
+            ffuf -c -w "$wordlist" -u "$protocol://$i" -H "Host: FUZZ" -fs "$filterrespsize" "$options" -H 'User-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:42.0) Gecko/20100101 Firefox/42.0'  -s > hfuzz-results/$domain/step1-$i.txt 2>/dev/null
+        done
+    else
+        # fuzz one ip with the wordlist
+        filterrespsize=$(curl -s -k -I "$protocol://$ip" | grep -i Content-Length | cut -f2 -d' ' | tr -d '\r\n')
+        ffuf -c -w "$wordlist" -u "$protocol://$ip" -H "Host: FUZZ" -fs "$filterrespsize" "$options" -H 'User-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:42.0) Gecko/20100101 Firefox/42.0'  -s > hfuzz-results/$domain/step1.txt 2>/dev/null
+    fi
+    
+fi
 #=======================================================================
 
 
 # Step 2 : static wordlist+domain.tld as host header
 #=======================================================================
 echo -e "   ${green}[+]${NC} Fuzzing step 2 - fuzzing method -> host: <word>.domain.tld"
-ffuf -w $wordlist -u "https://domaint.tld" -H "host: FUZZ.$domain" -H 'User-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:42.0) Gecko/20100101 Firefox/42.0' $thread $match_code $filter_code -s -o results/$domain/step2.txt
+if ([ -z "$ip" ] && [ -z "$ips" ]) || ([ -z "$wordlist" ]) || ([ -z "$domain" ]);then
+    echo -e "Please define Ip, wordlist and Domain"
+    usage
+else
+    if [ -z "$ip" ];then
+        # fuzz list of ips with the wordlist
+        for i in $(cat "$ips");do
+            # with this filter we want to remove those are showing the same website as default
+            filterrespsize=$(curl -s -k -I "$protocol://$i" | grep -i Content-Length | cut -f2 -d' ' | tr -d '\r\n')
+            ffuf -c -w "$wordlist" -u "$protocol://$i" -H "Host: FUZZ.$domain" -fs "$filterrespsize" "$options" -H 'User-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:42.0) Gecko/20100101 Firefox/42.0'  -s > hfuzz-results/$domain/step2-$i.txt   2>/dev/null
+        done
+    else
+        # fuzz one ip with the wordlist
+        filterrespsize=$(curl -s -k -I "$protocol://$ip" | grep -i Content-Length | cut -f2 -d' ' | tr -d '\r\n')
+        ffuf -c -w "$wordlist" -u "$protocol://$ip" -H "Host: FUZZ.$domain" -fs "$filterrespsize" "$options" -H 'User-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:42.0) Gecko/20100101 Firefox/42.0'  -s > hfuzz-results/$domain/step2.txt 2>/dev/null
+    fi
+    
+fi
 #=======================================================================
 
 
-# Step 3 : only static wordlist as host header
+# Step 3 : subdomians as host header
 #=======================================================================
 echo -e "   ${green}[+]${NC} Fuzzing step 3 - fuzzing method -> host: <subdomain>"
-ffuf -w $subdomain -u "https://domaint.tld" -H "host: FUZZ" -H 'User-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:42.0) Gecko/20100101 Firefox/42.0' $thread $match_code $filter_code -s -o results/$domain/step3.txt
+if ([ -z "$ip" ] && [ -z "$ips" ]) || ([ -z "$swordlist" ]) ;then
+    echo -e "Please define Ip and Subdomain wordlist"
+    usage
+else
+    if [ -z "$ip" ];then
+        # fuzz list of ips with the wordlist
+        for i in $(cat "$ips");do
+            # with this filter we want to remove those are showing the same website as default
+            filterrespsize=$(curl -s -k -I "$protocol://$i" | grep -i Content-Length | cut -f2 -d' ' | tr -d '\r\n')
+            ffuf -c -w "$swordlist" -u "$protocol://$i" -H "Host: FUZZ" -fs "$filterrespsize" "$options" -H 'User-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:42.0) Gecko/20100101 Firefox/42.0'  -s > hfuzz-results/$domain/step3-$i.txt 2>/dev/null
+        done
+    else
+        # fuzz one ip with the wordlist
+        filterrespsize=$(curl -s -k -I "$protocol://$ip" | grep -i Content-Length | cut -f2 -d' ' | tr -d '\r\n')
+        ffuf -c -w "$swordlist" -u "$protocol://$ip" -H "Host: FUZZ" -fs "$filterrespsize" "$options" -H 'User-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:42.0) Gecko/20100101 Firefox/42.0'  -s > hfuzz-results/$domain/step3.txt 2>/dev/null
+    fi
+    
+fi
 #=======================================================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
